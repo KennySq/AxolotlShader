1c4252d1263354a7f966bf4320bda1aedbc6c044
Kenny91 <odessa5d4@gmail.com>
2021-12-27 17:44:49 +0900

    Parser ToString update

--- AxolotlShader/inc/AXParser.h
+++ AxolotlShader/inc/AXParser.h
@@ -2,12 +2,24 @@
 
 struct AXParser
 {
+	struct ByteBlock
+	{
+		std::vector<unsigned char> Bytes;
+		size_t From;
+		size_t To;
+
+		std::string ToString() const;
+		std::string ToNumericString(int base) const;
+	};
+
+
 public:
 	AXParser(const char* path);
 	~AXParser();
 	static std::shared_ptr<AXParser> OpenFile(const char* path);
 
-	std::string Read(size_t from, size_t to);
+	ByteBlock Read(size_t from, size_t to);
+	ByteBlock Read(size_t from); // this method will reads data until encounters 0x00 (NULL).
 	char Read();
 private:
 
--- AxolotlShader/src/AXParser.cpp
+++ AxolotlShader/src/AXParser.cpp
@@ -23,29 +23,64 @@ std::shared_ptr<AXParser> AXParser::OpenFile(const char* path)
 	return std::make_shared<AXParser>(path);
 }
 
-std::string AXParser::Read(size_t from, size_t to)
+AXParser::ByteBlock AXParser::Read(size_t from, size_t to)
 {
 	mFile.seekg(from);
 
-	std::string buffer;
+	ByteBlock block;
+
+	block.From = from;
+	block.To = to;
+
 	int count = to - from;
 	int index = 0;
 
-	buffer.resize(count);
+	block.Bytes.resize(count);
 
 	while (index < count)
 	{
 		int buf;
 		buf = mFile.get();
 
-		buffer[index++] = buf;
+		block.Bytes[index++] = buf;
 	}
 
 	mLast = to;
 
 	jump();
 
-	return buffer;
+	return block;
+}
+
+AXParser::ByteBlock AXParser::Read(size_t from)
+{
+	mFile.seekg(from);
+
+	ByteBlock block;
+
+	block.From = from;
+
+	int index = 0;
+
+	while (true)
+	{
+		int buf;
+		buf = mFile.get();
+
+		if (buf == 0x00)
+		{
+			break;
+		}
+
+		block.Bytes.push_back(buf);
+	}
+
+	mLast = block.From + index;
+	block.To = mLast;
+
+	jump();
+
+	return block;
 }
 
 char AXParser::Read()
@@ -74,4 +109,38 @@ void AXParser::jump()
 	}
 
 
+}
+
+std::string AXParser::ByteBlock::ToString() const
+{
+	std::string str(Bytes.begin(), Bytes.end());
+
+	return str;
+}
+
+std::string AXParser::ByteBlock::ToNumericString(int base) const
+{
+	std::string str;
+	size_t size = Bytes.size();
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		char buf[256];
+		_itoa_s(Bytes[i], buf, 256, base);
+		
+		for (unsigned int j = 0; j < 256; j++)
+		{
+
+			if (buf[j] == '\0')
+			{
+				break;
+			}
+			str.push_back(buf[j]);
+			
+		}
+
+		str.push_back(' ');
+	}
+	
+	return str;
 }
