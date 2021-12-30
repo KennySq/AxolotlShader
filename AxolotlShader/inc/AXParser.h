650975a4cefb15b06154da9132f8f88db18735ac
Kenny91 <odessa5d4@gmail.com>
2021-12-30 17:12:55 +0900

    AXParser update

--- AxolotlShader/inc/AXParser.h
+++ AxolotlShader/inc/AXParser.h
@@ -18,16 +18,79 @@ public:
 	~AXParser();
 	static std::shared_ptr<AXParser> OpenFile(const char* path);
 
-	ByteBlock Read(size_t from, size_t to);
-	ByteBlock Read(size_t from); // this method will reads data until encounters 0x00 (NULL).
-	char Read();
+	inline void Jump(size_t offset)
+	{
+		mIndex += offset;
+	}
+
+	inline void Jump(size_t from, size_t offset)
+	{
+		mIndex = from + offset;
+	}
+
+	template<typename _Ty>
+	_Ty Read()
+	{
+		assert(mIndex + sizeof(_Ty) < mSize);
+
+		_Ty result = *reinterpret_cast<const _Ty*>(&mRaw[mIndex]);
+	
+		mIndex += sizeof(_Ty);
+
+		return result;
+	}
+
+	std::string ReadString()
+	{
+		std::string ret;
+
+		unsigned int index = mIndex;
+
+		while (true)
+		{
+			char* ch = reinterpret_cast<char*>(&mRaw[index]);
+
+			if (*ch == 0x00)
+			{
+				break;
+			}
+
+			ret.push_back(*ch);
+
+			index++;
+		}
+
+		mIndex = index + 1;
+
+		return ret;
+	}
+
+	uint8_t ReadUint8()
+	{
+		return Read<uint8_t>();
+	}
+
+	uint16_t ReadUint16()
+	{
+		return Read<uint16_t>();
+	}
+
+	uint32_t ReadUint32()
+	{
+		return Read<uint32_t>();
+	}
+	uint64_t ReadUint64()
+	{
+		return Read<uint64_t>();
+	}
+
+
 private:
-
-	void jump();
-
 	char mByte;
-	size_t mLast;
+	
+	unsigned char* mRaw;
+	size_t mIndex;
+	size_t mSize;
 
 	std::ifstream mFile;
-	unsigned int mReadIndex;
 };
--- AxolotlShader/src/AXParser.cpp
+++ AxolotlShader/src/AXParser.cpp
@@ -1,9 +1,9 @@
-#include"pch.h"
+#pragma once
+#include<pch.h>
 #include"AXParser.h"
 
-#include"RDEF.h"
-
 AXParser::AXParser(const char* path)
+	: mIndex(0)
 {
 	mFile.open(path, std::ifstream::binary);
 	
@@ -12,6 +12,19 @@ AXParser::AXParser(const char* path)
 		std::cout << "Failed.\n";
 	}
 
+	mFile.seekg(0, mFile.end);
+
+	unsigned int binarySize = mFile.tellg();
+
+	mFile.seekg(0, mFile.beg);
+
+	mRaw = new unsigned char[binarySize];
+
+	mFile.read((char*)mRaw, binarySize);
+	mFile.close();
+	mSize = binarySize;
+
+	return;
 }
 
 AXParser::~AXParser()
@@ -21,94 +34,6 @@ AXParser::~AXParser()
 std::shared_ptr<AXParser> AXParser::OpenFile(const char* path)
 {
 	return std::make_shared<AXParser>(path);
-}
-
-AXParser::ByteBlock AXParser::Read(size_t from, size_t to)
-{
-	mFile.seekg(from);
-
-	ByteBlock block;
-
-	block.From = from;
-	block.To = to;
-
-	int count = to - from;
-	int index = 0;
-
-	block.Bytes.resize(count);
-
-	while (index < count)
-	{
-		int buf;
-		buf = mFile.get();
-
-		block.Bytes[index++] = buf;
-	}
-
-	mLast = to;
-
-	jump();
-
-	return block;
-}
-
-AXParser::ByteBlock AXParser::Read(size_t from)
-{
-	mFile.seekg(from);
-
-	ByteBlock block;
-
-	block.From = from;
-
-	int index = 0;
-
-	while (true)
-	{
-		int buf;
-		buf = mFile.get();
-
-		if (buf == 0x00)
-		{
-			break;
-		}
-
-		block.Bytes.push_back(buf);
-	}
-
-	mLast = block.From + index;
-	block.To = mLast;
-
-	jump();
-
-	return block;
-}
-
-char AXParser::Read()
-{
-	mFile.read(&mByte, 1);
-
-	return mByte;
-}
-
-void AXParser::jump()
-{
-	size_t idx = mLast;
-	while (mFile.eof() == false)
-	{
-		char b;
-		mFile.read(&b, 1);
-
-		if (b != 0x00)
-		{
-			mFile.seekg(idx);
-			mLast = idx;
-			break;
-		}
-
-		idx++;
-	}
-
-
 }
 
 std::string AXParser::ByteBlock::ToString() const
