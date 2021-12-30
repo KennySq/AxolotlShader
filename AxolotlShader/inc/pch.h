33ce7193ebdbae699fc943c99603882c1ca8d6e8
Kenny91 <odessa5d4@gmail.com>
2021-12-30 17:13:33 +0900

    pch, project update

--- AxolotlShader/inc/pch.h
+++ AxolotlShader/inc/pch.h
@@ -10,11 +10,15 @@
 // 여기에 미리 컴파일하려는 헤더 추가
 #include "framework.h"
 
+#include<Windows.h>
+
 #include<iostream>
 #include<fstream>
 
 #include<map>
 #include<vector>
 #include<string>
+
+#include<assert.h>
 
 #endif //PCH_H
