8c3aa54e5d6c0d552a9e3f47ab42e165a3dc2943
KennySq <odessa5d4@gmail.com>
2022-02-11 17:50:50 +0900

    OOP update

---
+++ AxolotlShader/inc/ShaderObject.h
@@ -0,0 +1,13 @@
+#pragma once
+struct ShaderObject
+{
+public:
+private:
+	size_t mParent;
+	size_t mOffset;
+	
+	std::string mName;
+
+	// type?
+
+};
---
+++ AxolotlShader/inc/ShaderVariable.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct ShaderVariable
+{
+public:
+
+private:
+
+	std::string mName;
+	uint32_t mVariableFlag;
+	uint32_t mVariableSize;
+	
+};
